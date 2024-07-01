51902ec457182bb6b8d442083024cbed1790a7b3
miguelmdsdp <miguelmartinsdossantos@gmail.com>
2024-07-01 18:11:52 -0300

    :bookmark: release: finish v3.1.0

--- src/containers/footer/index.tsx
+++ src/containers/footer/index.tsx
@@ -11,6 +11,7 @@ import useEfficientRansac from "../../hooks/processing/useEfficientRansac";
 
 /* UTILS */
 import { useTranslation } from "react-i18next";
+import { SOFTWARE_VERSION } from "../../utils/constants/version";
 
 const Footer: FC = () => {
   const { t } = useTranslation();
@@ -20,7 +21,7 @@ const Footer: FC = () => {
 
   return (
     <Row className="w-full h-16 bg-light-grey px-16">
-      <Col span={12}>
+      <Col span={8}>
         {isEfficientRansacApplied && (
           <Row justify="start" align="middle" className="h-16">
             <span className="font-bold text-brown text-base">
@@ -43,7 +44,10 @@ const Footer: FC = () => {
           </Row>
         )}
       </Col>
-      <Col span={12} className="h-16">
+      <Col span={8} className="flex items-end h-16 justify-center">
+        <span className="text-xs text-brown mb-3 ml-2 font-light">{`v${SOFTWARE_VERSION}`}</span>
+      </Col>
+      <Col span={8} className="h-16">
         <Row justify="end" align="middle" className="h-16">
           <span className="text-brown">
             <span className="font-semibold">Status: </span>
---
+++ src/utils/constants/version.ts
@@ -0,0 +1,1 @@
+export const SOFTWARE_VERSION = "3.1.0";
